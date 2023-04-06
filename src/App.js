d0915183ccfd3c277a832bd8289259b16300509e
hamzazahid8556205 <129792956+hamzazahid8556205@users.noreply.github.com>
2023-04-06 06:09:54 +0500

    react-router-Dom
    All exersises done According to the requirement

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Exercise1 from './Exercise1';
 import Exercise2 from './Exercise2';
 import Exercise3 from './Exercise3';
 import Exercise4 from './Exercise4';
-
+import AboutUs from './AboutUs';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
+    
     <div className="App">
-     
-  {/* <Exercise2 /> */}
-     {/* <Exercise1 /> */}
+      <nav>
+        
+  <Link to='/Exercise1'>Exercise1</Link>
+  <Link to='/Exercise2'>Exercise2</Link>
+  <Link to='/Exercise3'>Exercise3</Link>
+  <Link to='/Exercise4'>Exercise4</Link>
+  <Link to='/AboutUs'>About Us</Link>
+  
+</nav>
 
-      {/* <Exercise3 /> */ }
-      { <Exercise4 /> }
+
+
+      <Exercise1 />
+      
+
+
+      <Routes>
+        <Route path = '/Exercise1' element = {<Exercise1/>} />
+        <Route path = '/Exercise2' element = {<Exercise2/>} />
+        <Route path = '/Exercise3' element = {<Exercise3/>} />
+        <Route path = '/Exercise4' element = {<Exercise4/>} />
+        <Route path = '/AboutUs' element = {<AboutUs/>} />
+      </Routes>
+      
     </div>
   );
 }
